29366c9bf23ff80cd3150c3aed59e874207caedb
William Méndez <williamm@avalith.net>
2022-08-30 11:35:21 -0500

    Update main using the new auth method

--- main.go
+++ main.go
@@ -7,8 +7,15 @@ import (
 )
 
 func main() {
-	token := ""
-	cli := client.New(token)
+	//token := "" //se va a borrar
+	credentials := `{
+		"user_name": "new_user_1659988043",
+		"client_id": "01g28sVBNwGJDKz8QX2hRyH3mJQDyymk",
+		"client_secret_id": "y--SbJipBCbbcptm8u-vD3chIApD7thl0gC87TD5GH-8YCzXUsDFHCiQhMKNvw7h",
+		"grant_type": "client_credentials"
+	}`
+	cli := client.New(nil)
+	cli.Auth(credentials)
 	order := cli.GetOrder("052f288c-5c37-4760-971c-3613b140fde9")
 	fmt.Println(order)
 }
